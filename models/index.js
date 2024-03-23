8e1926180bf1919b016e048f1ec87db19fc36778
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-03-23 16:52:03 -0600

    All GET routes complete for tag

--- models/index.js
+++ models/index.js
@@ -14,9 +14,9 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: 'product_tag' });
+Product.belongsToMany(Tag, { through: { model: 'product_tag' }, foreignKey: 'product_id' });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: 'product_tag' })
+Tag.belongsToMany(Product, { through: { model: 'product_tag' }, foreignKey: 'tag_id' });
 
 module.exports = {
   Product,
