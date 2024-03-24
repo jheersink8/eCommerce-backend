975de60f17e67b3d569e828eadb18f7a293a3180
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-03-23 19:48:13 -0600

    Finished all POST routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,8 +36,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  // DEV UPDATE: Added async/await to route
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 router.put('/:id', (req, res) => {
